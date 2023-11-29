3a783af942edf374edc68916b05110d8570c4bf9
FerchaPombo <>
2023-11-29 14:10:47 +0000

    add post model 1st try

--- finditberlin/models.py
+++ finditberlin/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Model for Post and Comment 
+
+class Post(model.Models):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default="placeholder", blank=False)
+    street_name = models.CharField(max_length=100)
+    street_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    likes = models.ManyToManyField(User, related_name = 'likes', blank=True)
+    slug = models.SlugField(unique=True)
+    approved = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse ('post_detail', args=[str(self.slug)])
+
