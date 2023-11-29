1f9e55ae6d5c40b27e51fafc376697a5824c0274
FerchaPombo <>
2023-11-29 14:50:56 +0000

    add oaths inside myblog

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import PostList, PostDetail, like_post, add_comment
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', PostList.as_view(), name='post_list'),
+    path('post<slug:slug>/', PostDetail.as.view(), name='post_detail'),
+    path('like/<int:pk>/', like_post, name='like_post'),
+    path('post/<slug:slug>/comment', add_comment, name='add_comment'),
 ]
